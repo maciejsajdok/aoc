b387a7de2ef26c368fd905fbf0d867c9672df56a
Maciej Sajdok <7015345+maciejsajdok@users.noreply.github.com>
2024-11-30 17:24:20 +0100

    2015 5

---
+++ app/Aoc/Year2015/Solution05.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Aoc\Year2015;
+
+use App\Services\Aoc\SolutionInterface;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use function array_count_values;
+use function array_values;
+use function count;
+use function dd;
+use function dump;
+use function explode;
+use function preg_match;
+use function preg_match_all;
+use function str_split;
+
+class Solution05 implements SolutionInterface
+{
+    public function p1(string $input): mixed
+    {
+        $strings = explode("\n", $input);
+        $okAmount = 0;
+        foreach ($strings as $string) {
+            $isOk = $this->hasThreeVowels($string)
+            && $this->containsDoubleLetter($string)
+            && $this->doesNotContainStrings($string);
+            if ($isOk) {
+                $okAmount++;
+            }
+        }
+
+        return $okAmount;
+    }
+
+    public function p2(string $input): mixed
+    {
+        $strings = explode("\n", $input);
+        $okAmount = 0;
+        foreach ($strings as $string) {
+            $isOk = $this->containsLetterPairThatOccursTwice($string)
+            && $this->containsOneLetterRepeatWithLetterInside($string);
+            if ($isOk) {
+                $okAmount++;
+            }
+        }
+
+        return $okAmount;
+    }
+
+    private function hasThreeVowels(string $input): bool
+    {
+        if (preg_match_all('/[aeiou]/', $input, $matches)) {
+            return count($matches[0]) >= 3;
+        }
+        return false;
+    }
+
+    private function containsDoubleLetter(string $input): bool
+    {
+        if (preg_match_all('/(.)\1+/', $input, $matches))
+        {
+            if (count($matches) > 1){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function doesNotContainStrings(string $input): bool
+    {
+        $needle = ['ab','cd','pq','xy'];
+
+        return !Str::contains($input, $needle);
+    }
+
+    private function containsLetterPairThatOccursTwice(string $input): bool
+    {
+        return preg_match('/(\w\w).*\1/', $input) === 1;
+    }
+
+    private function containsOneLetterRepeatWithLetterInside(string $input): bool
+    {
+        return preg_match('/(\w).\1/', $input) === 1;
+    }
+}
