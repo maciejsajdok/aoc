a34435272d6a3d970a6349eb5351c8839a62d6a9
Maciej Sajdok <7015345+maciejsajdok@users.noreply.github.com>
2024-12-03 20:25:59 +0100

    2015 21 init

---
+++ app/Aoc/Year2015/Solution21.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Aoc\Year2015;
+
+use App\Services\Aoc\SolutionInterface;
+
+class Solution21 implements SolutionInterface
+{
+    public function p1(string $input): mixed
+    {
+        return null;
+    }
+
+    public function p2(string $input): mixed
+    {
+        return null;
+    }
+}
