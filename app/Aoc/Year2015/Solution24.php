bd27e30593466e9de7eb3b791ebcf3b25713608b
Maciej Sajdok <maciej.sajdok@tsh.io>
2024-12-07 21:06:10 +0100

    2015 24 start

--- app/Aoc/Year2015/Solution24.php
+++ app/Aoc/Year2015/Solution24.php
@@ -8,6 +8,7 @@ use App\Services\Aoc\SolutionInterface;
 use App\Utilities\Combinations;
 use function array_product;
 use function array_sum;
+use function count;
 use function dd;
 use function explode;
 use const PHP_INT_MAX;
@@ -21,11 +22,10 @@ class Solution24 implements SolutionInterface
         foreach (explode("\n", trim($input)) as $line) {
             $weights[] = (int)trim($line);
         }
-        $part = 520;
+        $part = array_sum($weights)/3;
         $combinations = new Combinations($weights);
         $minimum = PHP_INT_MAX;
-        foreach ($combinations->getCombinations(count($weights)) as $combination) {
-            dd('test');
+        foreach ($combinations->getCombinations(6) as $combination) {
             if (array_sum($combination) === $part){
                 $minimum = min(array_product($combination), $minimum);
             }
@@ -36,6 +36,20 @@ class Solution24 implements SolutionInterface
 
     public function p2(string $input): mixed
     {
-        return null;
+        $weights = [];
+
+        foreach (explode("\n", trim($input)) as $line) {
+            $weights[] = (int)trim($line);
+        }
+        $part = array_sum($weights)/4;
+        $combinations = new Combinations($weights);
+        $minimum = PHP_INT_MAX;
+        foreach ($combinations->getCombinations(3) as $combination) {
+            if (array_sum($combination) === $part){
+                $minimum = min(array_product($combination), $minimum);
+            }
+        }
+
+        return $minimum;
     }
 }
