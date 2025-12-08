d431dae8cfe393c44ac24549d54b11f95cbee5eb
Maciej Sajdok <maciej.sajdok@tsh.io>
2025-12-08 07:45:50 +0100

    2025 8

--- app/Aoc/Year2025/Solution08.php
+++ app/Aoc/Year2025/Solution08.php
@@ -120,6 +120,7 @@ class Solution08 implements SolutionInterface
             [$left, $right] = explode(';', $pairKey);
 
             $merged = $union($left, $right);
+            dump($parent);
             if (!$merged) {
                 continue;
             }
