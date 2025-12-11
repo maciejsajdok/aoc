d611dd9a083209bb7efb5a17324789f777698245
Maciej Sajdok <maciej.sajdok@tsh.io>
2025-12-11 06:33:18 +0100

    2025 11 p2

--- app/Aoc/Year2025/Solution11.php
+++ app/Aoc/Year2025/Solution11.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Aoc\Year2025;
 
 use App\Services\Aoc\SolutionInterface;
+use App\Utilities\Memoize;
 use function array_shift;
 use function explode;
+use function in_array;
 
 class Solution11 implements SolutionInterface
 {
@@ -63,32 +65,23 @@ class Solution11 implements SolutionInterface
             $servers[$source] = $segments;
         }
 
-        $start = 'you';
-        $queue = [
-            [
-                $start,
-                []
-            ]
-        ];
-        $paths = [];
-        $head = 0;
-        while($head < count($queue)){
-            $queueItem = $queue[$head++];
-            $nextNode = $queueItem[0];
-            $pathSoFar = $queueItem[1];
-            $pathSoFar[] = $nextNode;
-            if($nextNode === 'out'){
-                $paths[] = $pathSoFar;
-                continue;
-            }
+        $solver = Memoize::make(function (string $node, bool $dac, bool $fft) use (&$solver, $servers){
+           if($node === 'out'){
+               if ($dac && $fft){
+                   return 1;
+               } else {
+                   return 0;
+               }
+           }
 
-            foreach ($servers[$nextNode] as $target) {
-                $queue[] = [
-                    $target, $pathSoFar
-                ];
-            }
-        }
+           $pathsCount = 0;
+           foreach ($servers[$node] as $target) {
+               $pathsCount += $solver($target, $dac || $target==='dac', $fft || $target==='fft');
+           }
 
-        return count($paths);
+           return $pathsCount;
+        });
+
+        return $solver('svr', false, false);
     }
 }
